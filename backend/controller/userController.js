32084f37f9fbbe97dc7da116d9aa7a21e1ad9d05
Ansh-Srivastava-1425 <kumar.ansh7591@gmail.com>
2026-06-01 18:49:21 +0530

    user controller

---
+++ backend/controller/userController.js
@@ -0,0 +1,78 @@
+    import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+    import ErrorHandler from "../middlewares/error.js";
+    import { User } from "../models/userSchema.js";
+    import { v2 as cloudinary } from "cloudinary";
+
+    export const register = catchAsyncError(async (req, res, next) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return next(new ErrorHandler("Avatar and Resume are required", 400));
+    }
+
+    const { avatar, resume } = req.files;
+
+    if (!avatar || !resume) {
+        return next(new ErrorHandler("Avatar and Resume are required", 400));
+    }
+
+    const cloudinaryResponseForAvatar = await cloudinary.uploader.upload(
+        avatar.tempFilePath,
+        {
+        folder: "AVATARS",
+        },
+    );
+
+    if (!cloudinaryResponseForAvatar || cloudinaryResponseForAvatar.error) {
+        return next(new ErrorHandler("Failed to upload avatar to Cloudinary", 500));
+    }
+
+    const cloudinaryResponseForResume = await cloudinary.uploader.upload(
+        resume.tempFilePath,
+        {
+        folder: "RESUMES",
+        resource_type: "raw", // important for PDFs, DOCX, etc.
+        },
+    );
+    if (!cloudinaryResponseForResume || cloudinaryResponseForResume.error) {
+        return next(new ErrorHandler("Failed to upload resume to Cloudinary", 500));
+    }
+
+    const {
+        fullName,
+        email,
+        phone,
+        aboutMe,
+        password,
+        portfolioURL,
+        githubURL,
+        instagramURL,
+        twitterURL,
+        facebookURL,
+        linkedInURL,
+    } = req.body;
+
+    const user = await user.create({fullName,
+        email,
+        phone,
+        aboutMe,
+        password,
+        portfolioURL,
+        githubURL,
+        instagramURL,
+        twitterURL,
+        facebookURL,
+        linkedInURL,
+        avatar:{
+            public_id:cloudinaryResponseForAvatar.public_id,
+            url:cloudinaryResponseForAvatar.secure_url,
+        },
+        resume:{
+            public_id:cloudinaryResponseForResume.public_id,
+            url:cloudinaryResponseForResume.secure_url,
+        },
+    })
+
+    res.status(200).json({
+        success: true,
+        message :"user Registered",
+    })
+    });
